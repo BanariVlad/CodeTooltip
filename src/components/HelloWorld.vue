b01a96c721e942f5461952e58846d9037c4ade2c
BanariVlad <banari.vlad@yandex.com>
2020-07-12 21:53:54 +0300

    Add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <v-container>
+    <v-tooltip right>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">Left</v-btn>
+      </template>
+      <span v-code-tooltip="code"></span>
+    </v-tooltip>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
-</script>
+  export default {
+    name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      code: `
+<!Doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div class="abc" id="yes">
+    <button>Hi</button>
+    <input type="text" placeholder="No" readonly>
+    <p>Never respect for others what you would not emerge for the dogma of it. Omnia varius caesium est.
+      Never respect for others what you would not emerge for the dogma of it. Omnia varius caesium est.
+      Never respect for others what you would not emerge for the dogma of it. Omnia varius caesium est.</p>
+    <div><span>a</span></div>
+  </div>
+</body>
+</html>
+      `
+    })
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,48 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
+Vue.directive("code-tooltip", {
+  bind(el, binding) {
+    let pre = document.createElement("pre");
+    let tagColor = "#FF6766"
+    let attributeColor = "#FECA6A"
+    let equalsColor = "#C2E782"
+    // let textColor = "#FFFFFF"
+    let closingColor = "#88D6E7"
+    pre.innerHTML = binding.value.replace(/(<.+?)(?=\s|>)|[<>]|="(.*?)"|(\w+(?==")|\w+-\w+(?=="))|\//g, value => {
+      // if (/>(.*?)<\//.test(value)) { // Color for text
+      //   return `<span style="color: ${closingColor};">${value.slice(0, 1)}</span><span style="color: ${textColor};">${value.slice(1, value.length - 2)}</span><span style="color: ${closingColor};"><</span><span style="color: ${closingColor};">/</span>`
+      // }
+
+      if (/(?<=<)(.+?)(?=\b)/.test(value)) { // Color for tags
+        if (value[1] === "/") {
+          return `<span style="color: ${closingColor};"><</span><span style="color: ${closingColor};">/</span><span style="color: ${tagColor};">${value.slice(2, value.length)}</span>`
+        }
+        return `<span style="color: ${closingColor};"><</span><span style="color: ${tagColor};">${value.slice(1, value.length)}</span>`
+      }
+
+      if (!/\w+/.test(value) || value === "/") { // Color for < / >
+        return `<span style="color: ${closingColor};">${value}</span>`
+      }
+
+      if (/="(.*?)"/.test(value)) { // Color for ="..."
+        return `<span style="color: ${equalsColor};">${value}</span>`
+      }
+
+      if (/\w+/.test(value)) { // Color for attribute
+        return `<span style="color: ${attributeColor};">${value}</span>`
+      }
+    });
+    el.appendChild(pre);
+  }
+});
+
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
